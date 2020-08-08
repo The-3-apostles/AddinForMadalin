710470599b78329f74c15cd9d762c3aabb0eabc3
Andrei Daha <andrei.daha@cnmv.ro>
2020-08-08 22:09:15 +0300

    Project test

---
+++ Code/MacroCs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MacroCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolidEdgeFramework.Application application = null;
+            Type type = null;
+            try
+            {
+                // Get the type from the Solid Edge ProgID
+                type = Type.GetTypeFromProgID("SolidEdge.Application");
+                // Start Solid Edge
+                application = (SolidEdgeFramework.Application)
+                Activator.CreateInstance(type);
+                // Make Solid Edge visible
+                application.Visible = true;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (application != null)
+                {
+                    Marshal.ReleaseComObject(application);
+                    application = null;
+                }
+            }
+        }
+    }
+}
